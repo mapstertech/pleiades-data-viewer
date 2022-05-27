fd11b9d415fc5a20fe6646c7d47d88d00aa5acd3
Victor Temprano <tempranova@gmail.com>
2022-05-27 00:55:24 -0700

    update dataset and display

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 import fetch from 'node-fetch';
 
+let reducedFeatures = [];
 fetch('./pleiades-places.csv').then(resp => resp.text()).then(response => {
   console.log(response);
+  // latitude, longitude, geoContent
+  response.forEach(item => {
+    reducedFeatures.push({
+      c : [parseFloat(item.longitude), parseFloat(item.latitude)],
+      n : item.geoContext
+    })
+  })
+  console.log(reducedFeatures);
 })
--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 
   const minDate = -2000;
   const maxDate = 1500;
+  let currentSelected = false;
+
+  /* Making mini file */
+  // /*
+  let reducedFeatures = [];
+  fetch('./files/pleiades-places.csv').then(resp => resp.text()).then(response => {
+    console.log(response);
+    csv({ output: "json"}).fromString(response).then(function(result){
+     console.log(result)
+     // latitude, longitude, geoContent
+     result.forEach(item => {
+       if(item.geoContext !== "" && item.longitude !== "" && item.latitude !== "") {
+         if(!reducedFeatures.find(feature => feature.n === item.title)) {
+           reducedFeatures.push({
+             c : [parseFloat(item.longitude), parseFloat(item.latitude)],
+             n : item.title
+           })
+         }
+       }
+     })
+     console.log(reducedFeatures);
+    })
+  })
+  // */
+
+  let miniLocationsFile = false;
+  fetch('./files/pleiades-min-locations.json').then(resp => resp.json()).then(response => {
+    miniLocationsFile = response;
+  })
 
   const placetypeValues = [ "abbey", "abbey-church", "acropolis", "agora", "plaza", "amphitheatre", "anchorage", "aqueduct", "arch", "archaeological-site", "archipelago", "architecturalcomplex", "basilica", "bath", "bay", "bridge", "building", "cairn", "canal", "cape", "cascade", "cemetery", "rapid", "causeway", "cave", "centuriation", "ceramicproduction", "church-2", "church", "circus", "cistern", "citadel", "city-block", "city-center", "coast", "coastal-change", "cultural-landscape", "dam", "delta", "deme-attic", "desert", "diocese-roman", "district", "earthwork", "ekklesiasterion", "escarpment", "estate", "estuary", "false", "findspot", "forest", "fort-2", "fort-group", "fort", "fortlet", "forum", "fountain", "frontier-system-limes", "garden-hortus", "city-gate", "gateway", "grove", "gymnasium", "hill", "hillfort", "hunting-base", "island", "isthmus", "lagoon", "lake", "league", "lighthouse", "marsh-wetland", "milestone", "military-installation-or-camp-temporary", "mine-2", "mine", "monastery", "monument", "mosque", "mountain", "nome-gr", "nuraghe", "oasis", "odeon", "palace", "palace-complex", "palaistra", "pass", "peninsula", "people", "piscina-roman", "plain", "plateau", "port", "postern", "production", "province-2", "province", "pyramid", "quarry", "regio-augusti", "region", "reservoir", "mouth", "river", "road", "room", "ruin", "salt-pan-salina", "salt-marsh", "sanctuary", "satrapy", "settlement", "fortified-settlement", "settlement-modern", "shrine", "space-interior", "space-uncovered", "spring", "stadion", "state", "station", "stoa", "street", "synagogue", "taberna-shop", "tell", "temple-2", "temple", "theatre", "tomb", "tower-defensive", "tower-single", "tower-wall", "townhouse", "treasury", "tribus", "tumulus", "tunnel", "undefined", "underground-structures", "unknown", "unlocated", "urban", "valley", "vicus", "villa", "volcano", "wall-2", "defensive-wall", "city-wall", "wall", "water-inland", "water-feature", "wheel", "water-open", "well", "whirlpool", "ziggurat" ];
 
@@ -18,7 +47,27 @@
   const geocoder = new MapboxGeocoder({
     accessToken: mapboxgl.accessToken,
     mapboxgl: mapboxgl,
-    placeholder : "Search for a modern-day area to zoom in closer."
+    placeholder : "Search for an ancient or modern location...",
+    localGeocoder : (query) => {
+      let matches = [];
+      miniLocationsFile.forEach(location => {
+        if(location.n && (location.n.toLowerCase().indexOf(query.toLowerCase()) > -1 || query === ' ')) {
+          var match = {
+            type : "Feature",
+            id : Math.random() * 1000000,
+            place_name : location.n,
+            center : location.c
+          };
+          // if(feature.geometry.type !== 'Point') {
+          //   match['bbox'] = turf.bbox(feature);
+          // }
+          matches.push(match)
+        }
+      })
+      console.log(matches)
+      matches.sort((a, b) => a.place_name > b.place_name ? 1 : -1)
+      return matches;
+    }
   })
   document.getElementById('autocomplete').appendChild(geocoder.onAdd(map));
 
@@ -68,10 +117,25 @@
 
   map.on('load', () => {
 
+    const images = ['bridge', 'building', 'cemetery', 'cross', 'ferry', 'monument', 'religious-christian', 'religious-muslim'];
+    images.forEach(imageUrl => {
+      let img = new Image(20,20)
+      img.onload = () => {
+        if(!map.hasImage(img.propertyName)) {
+          map.addImage(img.propertyName, img)
+        }
+      }
+      img.propertyName = imageUrl;
+      img.src = `./img/${imageUrl}.svg`;
+    })
+
     map.addSource('pleiades_places', {
       type : 'vector',
       url : 'mapbox://mapstertech.pleiades_places'
     });
+
+    // district, settlement, settlement (fortified), settlement-modern, estate, villa, port, water
+    // province, state (polity), temple, tomb, "urban area", "province, diocese, nome", regio, region,
 
     map.addLayer({
       'id' : 'pleiades_places_symbols',
@@ -89,7 +153,25 @@
           "mountain",
           ['in', 'mountain', ['get', 'featureTypes']],
           "mountain",
-          "none"
+          ['in', 'bridge', ['get', 'featureTypes']],
+          "bridge",
+          ['in', 'building', ['get', 'featureTypes']],
+          "building",
+          ['in', 'tomb', ['get', 'featureTypes']],
+          "cemetery",
+          ['in', 'port', ['get', 'featureTypes']],
+          "ferry",
+          ['in', 'monument', ['get', 'featureTypes']],
+          "monument",
+          ['in', 'church', ['get', 'featureTypes']],
+          "religious-christian",
+          ['in', 'abbey', ['get', 'featureTypes']],
+          "religious-christian",
+          ['in', 'monastery', ['get', 'featureTypes']],
+          "religious-christian",
+          ['in', 'mosque', ['get', 'featureTypes']],
+          "religious-muslim",
+          "dot-11"
         ],
         'icon-allow-overlap' : true,
         'icon-ignore-placement' : true,
@@ -120,17 +202,15 @@
           "#FFFFFF"
         ],
       },
-      'filter' : [
-        'any',
-        ['in', 'settlement', ['get', 'featureTypes']],
-        ['in', 'forest', ['get', 'featureTypes']],
-        ['in', 'hill', ['get', 'featureTypes']],
-        ['in', 'mountain', ['get', 'featureTypes']]
-      ]
+    //   'filter' : [
+    //     'any',
+    //     ['in', 'settlement', ['get', 'featureTypes']],
+    //     ['in', 'forest', ['get', 'featureTypes']],
+    //     ['in', 'hill', ['get', 'featureTypes']],
+    //     ['in', 'mountain', ['get', 'featureTypes']]
+    //   ]
     })
 
-    // district, settlement, settlement (fortified), settlement-modern, estate, villa, port, water
-    // province, state (polity), temple, tomb, "urban area", "province, diocese, nome", regio, region,
     map.addLayer({
       'id' : 'pleiades_places_areas',
       'type' : 'symbol',
@@ -152,7 +232,7 @@
           ['in', 'district', ['get', 'featureTypes']],
           14,
           14
-        ],
+        ]
       },
       'paint' : {
         'text-halo-color' : '#FFF',
@@ -192,7 +272,7 @@
           ['in', 'villa', ['get', 'featureTypes']],
           11,
           10
-        ],
+        ]
       },
       'paint' : {
         'text-halo-color' : '#FFF',
@@ -214,6 +294,43 @@
     }, 'pleiades_places_areas');
 
     map.addLayer({
+      'id' : 'pleiades_places_other',
+      'type' : 'symbol',
+      'source' : 'pleiades_places',
+      'source-layer' : 'pleiades_places',
+      'layout' : {
+        'text-field' : ['get', 'title'],
+        'text-offset' : [0, 1],
+        'text-padding' : 10,
+        'text-size' : 10
+      },
+      'paint' : {
+        'text-halo-color' : '#FFF',
+        'text-halo-blur' : 1,
+        'text-halo-width' : 2,
+        'text-color' : [
+          'case',
+          ['boolean', ['feature-state', 'hover'], false],
+          "#1870d5",
+          "#000000"
+        ]
+      },
+      'filter' : [
+        'all',
+        ['!', ['in', 'settlement', ['get', 'featureTypes']]],
+        ['!', ['in', 'estate', ['get', 'featureTypes']]],
+        ['!', ['in', 'villa', ['get', 'featureTypes']]],
+        ['!', ['in', 'forest', ['get', 'featureTypes']]],
+        ['!', ['in', 'hill', ['get', 'featureTypes']]],
+        ['!', ['in', 'mountain', ['get', 'featureTypes']]],
+        ['!', ['in', 'state', ['get', 'featureTypes']]],
+        ['!', ['in', 'regio', ['get', 'featureTypes']]],
+        ['!', ['in', 'district', ['get', 'featureTypes']]],
+        ['!', ['in', 'province', ['get', 'featureTypes']]],
+      ]
+    }, 'pleiades_places_areas');
+
+    map.addLayer({
       'id' : 'pleiades_places_nature',
       'type' : 'symbol',
       'source' : 'pleiades_places',
@@ -231,7 +348,7 @@
           ['in', 'mountain', ['get', 'featureTypes']],
           10,
           10
-        ],
+        ]
       },
       'paint' : {
         'text-halo-color' : '#FFF',
@@ -255,7 +372,7 @@
     let hoveredStateId = null;
 
     map.on('mousemove', (e) => {
-      const features = map.queryRenderedFeatures(e.point, { layers : ['pleiades_places_areas', 'pleiades_places', 'pleiades_places_nature'] });
+      const features = map.queryRenderedFeatures(e.point, { layers : ['pleiades_places_areas', 'pleiades_places_other', 'pleiades_places', 'pleiades_places_nature'] });
       if(features.length >  0) {
         map.getCanvas().style.cursor = 'pointer';
 
@@ -321,19 +438,36 @@
     })
 
     map.on('click', (e) => {
-      const features = map.queryRenderedFeatures(e.point, { layers : ['pleiades_places_areas', 'pleiades_places', 'pleiades_places_nature'] });
+      const features = map.queryRenderedFeatures(e.point, { layers : ['pleiades_places_areas', 'pleiades_places_other', 'pleiades_places', 'pleiades_places_nature'] });
       if(features.length >  0) {
-        let html = "<table><thead><tr><th>Property</th><th>Value</th></thead><tbody>";
-        for(let prop in features[0].properties) {
-          html += `<tr><td>${prop}</td><td>${features[0].properties[prop]}</td></tr>`
+        $('#more-info').fadeIn();
+        currentSelected = features[0];
+        for(let prop in currentSelected.properties) {
+          if($(`#item-${prop}`).length) {
+            if(currentSelected.properties[prop] && currentSelected.properties[prop] !== "") {
+              $(`#item-${prop}`).html(currentSelected.properties[prop]);
+              $(`#item-${prop}`).show();
+            } else {
+              $(`#item-${prop}`).hide();
+            }
+          }
         }
-        html += "</tbody></table>";
-        $('#more-info').html(html)
+        $(`#item-link`).attr('href', `https://pleiades.stoa.org${currentSelected.properties.path}`);
 
-        const extent = JSON.parse(features[0].properties.extent);
+        const extent = JSON.parse(currentSelected.properties.extent);
         const bbox = turf.bbox({ type : "Feature", properties : {}, geometry : extent });
         map.fitBounds(bbox, { padding: 20, maxZoom : 12 })
       }
+    })
+
+    $(document).on('click', '#modal-info', () => {
+      $('.modal-title').text(currentSelected.properties.title);
+      let html = "<table><thead><tr><th>Property</th><th>Value</th></thead><tbody>";
+      for(let prop in currentSelected.properties) {
+        html += `<tr><td>${prop}</td><td>${currentSelected.properties[prop]}</td></tr>`
+      }
+      html += "</tbody></table>";
+      $('.modal-body').html(html);
     })
 
     $( function() {
@@ -346,8 +480,8 @@
           ["<=", ["to-number", ["get", "minDate"]], 0],
           [">=", ["to-number", ["get", "maxDate"]], 0]
         ],
-        1,
-        0
+        ['get', 'title'],
+        ''
       ]
       const iconExpression = [
         "case",
@@ -356,13 +490,14 @@
           ["<=", ["to-number", ["get", "minDate"]], 0],
           [">=", ["to-number", ["get", "maxDate"]], 0]
         ],
-        0.3,
+        1,
         0
       ]
-      map.setPaintProperty('pleiades_places_symbols', 'icon-opacity', iconExpression)
-      map.setPaintProperty('pleiades_places_areas', 'text-opacity', expression)
-      map.setPaintProperty('pleiades_places', 'text-opacity', expression)
-      map.setPaintProperty('pleiades_places_nature', 'icon-opacity', expression)
+      map.setLayoutProperty('pleiades_places_symbols', 'icon-size', iconExpression)
+      map.setLayoutProperty('pleiades_places_areas', 'text-field', expression)
+      map.setLayoutProperty('pleiades_places', 'text-field', expression)
+      map.setLayoutProperty('pleiades_places_nature', 'text-field', expression)
+      map.setLayoutProperty('pleiades_places_other', 'text-field', expression)
       $( "#slider" ).slider({
         min: minDate,
         max: maxDate,
@@ -375,8 +510,8 @@
               ["<=", ["to-number", ["get", "minDate"]], ui.value],
               [">=", ["to-number", ["get", "maxDate"]], ui.value]
             ],
-            1,
-            0
+            ['get', 'title'],
+            ''
           ]
           const iconExpression = [
             "case",
@@ -385,13 +520,14 @@
               ["<=", ["to-number", ["get", "minDate"]], ui.value],
               [">=", ["to-number", ["get", "maxDate"]], ui.value]
             ],
-            0.3,
+            1,
             0
           ]
-          map.setPaintProperty('pleiades_places_symbols', 'icon-opacity', iconExpression)
-          map.setPaintProperty('pleiades_places_areas', 'text-opacity', expression)
-          map.setPaintProperty('pleiades_places', 'text-opacity', expression)
-          map.setPaintProperty('pleiades_places_nature', 'icon-opacity', expression)
+          map.setLayoutProperty('pleiades_places_symbols', 'icon-size', iconExpression)
+          map.setLayoutProperty('pleiades_places_areas', 'text-field', expression)
+          map.setLayoutProperty('pleiades_places', 'text-field', expression)
+          map.setLayoutProperty('pleiades_places_nature', 'text-field', expression)
+          map.setLayoutProperty('pleiades_places_other', 'text-field', expression)
           $( "#time" ).val( returnReadableDate(ui.value) );
         }
       });
